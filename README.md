cc9d350e43a1400f32f8c7939c6f76099b5973e2
Conan Lee <liyijia@shinetechchina.com>
2017-05-11 17:56:18 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-avatar-image-cropper
---
+++ src/react-avatar-image-cropper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AvatarImageCropper extends Component {
+    render() {
+        return (
+            <avatar-image-cropper>
+                hello world!
+            </avatar-image-cropper>
+        );
+    }
+}
+
+export default AvatarImageCropper;
